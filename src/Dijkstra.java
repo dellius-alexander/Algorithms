d99db668c53fc801f8b2bdb4ecc2c4c87a17812f
[dellius-alexander] <dellius.alexander@gmail.com>
2020-03-17 02:28:56 -0400

    commit to master

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,137 +1,133 @@
-import java.io.File;
+/** ************************************************************************************************
+ * The Dijkstra's algorithm is by far the best shortest path algorithm
+ * This program illustrates the algorithm and program control flow of the data structure
+ * @author Dellius Alexander
+ *
+ * ALGORITHM: Dijkstra’s Algorithm
+ * (G = Graph: weighted connected simple graph, with all weights positive)
+ * {G has vertices a = v_0, v_1,… , v_n = z and lengths w(v_i , v_j )
+ *      where w(v_i , v_j ) = ∞ if {v_i , v_j } is not an edge in G}
+ * for i := 1 to n L(v_i ) := ∞
+ * L(a) := 0
+ * S := ∅
+ * {the labels are now initialized so that the label of a is 0 and all
+ *      other labels are ∞, and S is the empty set}
+ * while z ∉ S
+ *      u := a vertex not in S with L(u) minimal
+ *      S := S ∪ {u}
+ *      for all vertices v not in S
+ *          if L(u) + w(u, v) < L(v) then L(v) := L(u) + w(u, v)
+ *          {this adds a vertex to S with minimal label and updates the
+ *          labels of vertices not in S}
+ * return L(z) {L(z) = length of a shortest path from a to z}
+ * *************************************************************************************************
+ * Dijkstra's Algorithm:  Key Ideas
+ * We keep track of two sets of nodes.  One set of nodes is settled, the
+ * other set is on the frontier.  Terminology varies; these are my recommendations.
+ * Once  a  node  is settled,  we'll  know  the  shortest  distance  and
+ * best  path  to that node (from the chosen start node).  We'll repeat the
+ * following steps:
+ * 1.   Choose an appropriate node from the frontier.
+ * 2.   Settlethat node, and update distance/path information.
+ * 3.   If possible, add new nodes to the frontier.
+ * Once the frontier is empty, the algorithm terminates.  If we have a
+ * particular ending node in mind, we can stop once that node becomes settled.
+ *
+ * Step 1:  We begin by settling the start node (at distance 0 from itself)
+ *          Other nodes are settled in increasing order of distance (from start/parent node)
+ *
+ * Step 2:  After each settling, we update the value of the current best weighted
+ *          distance to each node, passing through only nodes that are already settled.
+ *          In other words, settled nodes function like intermediate nodes from Warshall/Floyd.
+ *
+ * Step 3:  When updating distances through settled nodes, we'll only need to
+ *          consider nodes for which there is an edge from the one that was just settled.
+ *          This is the key to entire algorithm.
+ *
+ * Step 4:  Instead of full paths, we keep a "parent/intermediate" for each node;
+ *          tracking back through these "parents/intermediate" nodes gives us the
+ *          shortest path from start;
+ *
+ * *************************************************************************************************
+ *
+ * DETAILED STEP BY STEP:
+ *  ■ EDGE: is defined as a connection between two nodes
+ *  ■ Frontier: is defined as any unsettled node that is "a visible edge from a settled
+ *      node"
+ *      (eg: after settling Node "F" the frontier nodes are now "C & G";
+ *      all other unsettled nodes are not considered because they are not on the
+ *      frontier of "F")
+ *
+ *  ■ Settled: a node who's distance is evaluated from a parent/root node
+ *  ■ Unsettled: an unevaluated node on the edges of settled nodes;
+ *               unsettled nodes are assigned avalue of infinity as
+ *               are not currently being evauated
+ *  ■ R = Root
+ *  ■ Start Node: "F"
+ *
+ * Note: Create 3 lists:
+ *              » Settled : a list of settled nodes
+ *              » Unsettled : a list of unsettled nodes
+ *              » Edges : a list of all edges
+ *
+ * Node:          A     B   C   D   E  "F"  G
+ * Distance:    | 11 | 10 | 5 | 7 | 5 | 0 | 1 |
+ * Parent:      |  B | D  | F | C | G | R | F |
+ *
+ *  1.  Start at the "ROOT" node; it has no parent so it will be
+ *          labeled as Root; the distance from Root to itself is "0"
+ *
+ *  1.2.For each round of setting any unsettled node gets a value of
+ *          infinity until its turn to be evaluated unless is it
+ *          part of our edge case evaluation
+ *
+ *  2.  Pick the closest edge(i,j) path with the shortest distance
+ *          or smallest weight; record the distance/weight in the
+ *          table and list its parent as the Root node or node
+ *          closest to Root node
+ *
+ *  3.  The next node to be settled is chosen from the frontier;
+ *          pick the one with the smallest distance; if their is a tie,
+ *          choose arbitrarily; this node is now removed from the
+ *          frontier, and added to the set of "settled" nodes, without
+ *          changing its distance or parent.
+ *  4.  After settling "N" nodes, we check to see if we have any
+ *          shortcuts through "N"; we can use "N" as an intermediate
+ *          node back to our Root node; in other words, we can only
+ *          travel paths through settled nodes only; we need only
+ *          consider the nodes that have an edge from "N"
+ *  5.  For each edge from "N" to an unsettled node "u", compute the
+ *          Set Distance(u): SUM = Distance(N) + edge_weight(u);
+ *          Set Parent(u):   to "N"
+ *
+ *
+ *************************************************************************************************/
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Dijkstra 
 {
-
-    /** ************************************************************************************************
-     * The Dijkstra's algorithm is by far the best shortest path algorithm
-     * This program illustrates the algorithm and program control flow of the data structure
-     * @author Dellius Alexander
-     *
-     * ALGORITHM: Dijkstra’s Algorithm
-     * (G = Graph: weighted connected simple graph, with all weights positive) 
-     * {G has vertices a = v_0, v_1,… , v_n = z and lengths w(v_i , v_j ) 
-     *      where w(v_i , v_j ) = ∞ if {v_i , v_j } is not an edge in G} 
-     * for i := 1 to n L(v_i ) := ∞ 
-     * L(a) := 0 
-     * S := ∅ 
-     * {the labels are now initialized so that the label of a is 0 and all 
-     *      other labels are ∞, and S is the empty set} 
-     * while z ∉ S 
-     *      u := a vertex not in S with L(u) minimal 
-     *      S := S ∪ {u} 
-     *      for all vertices v not in S 
-     *          if L(u) + w(u, v) < L(v) then L(v) := L(u) + w(u, v) 
-     *          {this adds a vertex to S with minimal label and updates the 
-     *          labels of vertices not in S} 
-     * return L(z) {L(z) = length of a shortest path from a to z}
-     * *************************************************************************************************
-     * Dijkstra's Algorithm:  Key Ideas 
-     * We keep track of two sets of nodes.  One set of nodes is settled, the 
-     * other set is on the frontier.  Terminology varies; these are my recommendations.  
-     * Once  a  node  is settled,  we'll  know  the  shortest  distance  and  
-     * best  path  to that node (from the chosen start node).  We'll repeat the  
-     * following steps:
-     * 1.   Choose an appropriate node from the frontier.
-     * 2.   Settlethat node, and update distance/path information.
-     * 3.   If possible, add new nodes to the frontier.
-     * Once the frontier is empty, the algorithm terminates.  If we have a 
-     * particular ending node in mind, we can stop once that node becomes settled.    
-     * 
-     * Step 1:  We begin by settling the start node (at distance 0 from itself)
-     *          Other nodes are settled in increasing order of distance (from start/parent node)
-     * 
-     * Step 2:  After each settling, we update the value of the current best weighted 
-     *          distance to each node, passing through only nodes that are already settled.
-     *          In other words, settled nodes function like intermediate nodes from Warshall/Floyd.
-     * 
-     * Step 3:  When updating distances through settled nodes, we'll only need to 
-     *          consider nodes for which there is an edge from the one that was just settled.  
-     *          This is the key to entire algorithm.
-     * 
-     * Step 4:  Instead of full paths, we keep a "parent/intermediate" for each node;  
-     *          tracking back through these "parents/intermediate" nodes gives us the
-     *          shortest path from start;
-     *
-     * *************************************************************************************************
-     *
-     * DETAILED STEP BY STEP:
-     *  ■ EDGE: is defined as a connection between two nodes
-     *  ■ Frontier: is defined as any unsettled node that is "a visible edge from a settled 
-     *      node"
-     *      (eg: after settling Node "F" the frontier nodes are now "C & G"; 
-     *      all other unsettled nodes are not considered because they are not on the 
-     *      frontier of "F")
-     * 
-     *  ■ Settled: a node who's distance is evaluated from a parent/root node
-     *  ■ Unsettled: an unevaluated node on the edges of settled nodes; 
-     *               unsettled nodes are assigned avalue of infinity as 
-     *               are not currently being evauated
-     *  ■ R = Root
-     *  ■ Start Node: "F"
-     * 
-     * Note: Create 3 lists: 
-     *              » Settled : a list of settled nodes
-     *              » Unsettled : a list of unsettled nodes
-     *              » Edges : a list of all edges
-     * 
-     * Node:          A     B   C   D   E  "F"  G
-     * Distance:    | 11 | 10 | 5 | 7 | 5 | 0 | 1 |
-     * Parent:      |  B | D  | F | C | G | R | F |
-     * 
-     *  1.  Start at the "ROOT" node; it has no parent so it will be 
-     *          labeled as Root; the distance from Root to itself is "0" 
-     * 
-     *  1.2.For each round of setting any unsettled node gets a value of
-     *          infinity until its turn to be evaluated unless is it 
-     *          part of our edge case evaluation
-     * 
-     *  2.  Pick the closest edge(i,j) path with the shortest distance
-     *          or smallest weight; record the distance/weight in the
-     *          table and list its parent as the Root node or node 
-     *          closest to Root node 
-     * 
-     *  3.  The next node to be settled is chosen from the frontier; 
-     *          pick the one with the smallest distance; if their is a tie,
-     *          choose arbitrarily; this node is now removed from the 
-     *          frontier, and added to the set of "settled" nodes, without 
-     *          changing its distance or parent.
-     *  4.  After settling "N" nodes, we check to see if we have any 
-     *          shortcuts through "N"; we can use "N" as an intermediate 
-     *          node back to our Root node; in other words, we can only 
-     *          travel paths through settled nodes only; we need only 
-     *          consider the nodes that have an edge from "N" 
-     *  5.  For each edge from "N" to an unsettled node "u", compute the 
-     *          Set Distance(u): SUM = Distance(N) + edge_weight(u); 
-     *          Set Parent(u):   to "N"
-     *          
-     * 
-     ****************************************************************************************/
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
     public final static int INF = 9999;
     private static Logger log = Logger.getLogger("Dijkstra");
     
     
-    /****************************************************************************************
+    /**********************************************************************************************
      * Default Constructor
-     ****************************************************************************************/
+     *********************************************************************************************/
     public Dijkstra(){}
-    /****************************************************************************************
+    /**********************************************************************************************
      * Dijkstra Algorihm
      * @param graph
      * @param startNode
-     ****************************************************************************************/
+     **********************************************************************************************/
     public Dijkstra(Integer graph[][], Integer startNode)
     {
         log.setLevel(Level.INFO);
@@ -156,7 +152,7 @@ public class Dijkstra
         // these nodes are removed from view as they are settled in the shortest path
         // from root
         Hashtable<String, Integer> frontier = new Hashtable<String, Integer>();
-        Integer nextShortestPath =0;
+        Integer nextShortestPathNode = 0;
         // Represents a single node and the weighted edges from its self (parent) to leaf nodes
         Integer[] node = null;
         List<Integer[][]> results = new ArrayList<Integer[][]>();
@@ -171,10 +167,10 @@ public class Dijkstra
                 node[j] = graph[i][j];
             }
             //System.out.println(printNode(node));
-            key = "Node "+(i+1)+":";
+            key = (i+1)+"";
             unsettledNodes.put(key,node);
         }
-       
+       printHash_1D(unsettledNodes);
         
         /**
          *  1.  Start at the "ROOT" node; it has no parent so it will be 
@@ -199,24 +195,28 @@ public class Dijkstra
         //System.out.println(printResults(setNodes));
 
 
-        /**
+        /*
          * 2.  After settling our starting node we need to look around
          *      on the frontier and find the smallest weighted edge.
          *      The node at that edge will be the shortest next path
-         *      to treverse along the graph.  For this graph we started
+         *      to traverse along the graph.  For this graph we started
          *      node "1" and the visible nodes (aka frontier nodes)
          *      are "2=1, 3=7 & 4=5"; the next shortest path to traverse
-             *      will be node "2" at a distance of "1"
-             *
-            Enumeration el = frontier.keys();
-            while (el.hasMoreElements()) {
-                String key = el.nextElement().toString();
-                int val = frontier.get(key);
-                System.out.println(key + " " + val);
+         *      will be node "2" at a distance of "1"
+         */
 
-            }
+            nextShortestPathNode = nextShortestPath(frontier);
+            System.out.println(frontier);
+            System.out.println(nextShortestPathNode);
 
 
+            frontier.remove("2");
+            System.out.println(frontier);
+
+            frontier.putAll(nodesOnFrontier(S, nextShortestPathNode, unsettledNodes));
+            System.out.println(frontier);
+
+            printHash_1D(unsettledNodes);
 
 /*
 
@@ -247,6 +247,121 @@ public class Dijkstra
 */
        
            
+    }
+    /**
+     * Removes a object pair from our hashtable and you provide
+     * the key
+     * @param nodeToRemove
+     * @param frontier
+     * @return
+     */
+    public Hashtable<String, Integer[]>  removeFromFrontier(Integer nodeToRemove, Hashtable<String, Integer[]>  frontier)
+    {
+        Enumeration el = frontier.keys();
+        String key = null;
+        Integer[] val = null;
+        while (el.hasMoreElements()) {
+            key = el.nextElement().toString();
+            val = frontier.get(key);
+            if(Integer.parseInt(key) == nodeToRemove){
+                frontier.remove(key);
+            }
+            printHash_1D(frontier);
+        }
+        return frontier;
+    }
+    /**
+     * Removes a object pair from our hashtable and you provide
+     * the key
+     * @param nodeToRemove
+     * @param frontier
+     * @return
+     */
+    public Hashtable<String, Integer>  removeFromFrontier(int nodeToRemove, Hashtable<String, Integer> frontier)
+    {
+        Enumeration el = frontier.keys();
+        String key = null;
+        int val = 0;
+        while (el.hasMoreElements()) {
+            key = el.nextElement().toString();
+            val = frontier.get(key);
+            if(Integer.parseInt(key) == nodeToRemove){
+                frontier.remove(key,val);
+            }
+            System.out.println(key + " " + val);
+        }
+        return frontier;
+    }
+
+    /**
+     * Get the next set of nodes to be added to the frontier
+     * @param root
+     * @param parentNode
+     * @param graph
+     * @return
+     */
+    public Hashtable<String, Integer> nodesOnFrontier(Integer root, Integer parentNode, Hashtable<String, Integer[]> hash)
+    {
+       Hashtable<String, Integer> frontier = new Hashtable<>();
+        int i = 0;
+        String key = "";
+        Integer[] val = null;
+        Enumeration el = hash.keys();
+        while (el.hasMoreElements())
+        {
+            key = el.nextElement().toString();
+
+            if(Integer.parseInt(key) == root)
+            {
+                hash.remove(key);
+            }
+            else if(Integer.parseInt(key) == parentNode )
+            {
+                    val = hash.get(key);
+                for (int j = 0; j < val.length; j++) {
+                    if(val[j] != INF && j != root && j != parentNode){
+                        frontier.put((j+1)+"",val[j]);
+                    }
+                }
+
+
+            }
+        }
+        return frontier;
+    }
+    /**
+     * Returns the node with the minimum distance/weight from the
+     * frontier
+     * @param hash  Hashtable consisting of <String, Integer> pair
+     * @return  The node with the minimum valued weight from the frontier
+     */
+    public Integer nextShortestPath(Hashtable<String, Integer> hash)
+    {
+        /*
+         * 2.  After settling our starting node we need to look around
+         *      on the frontier and find the smallest weighted edge.
+         *      The node at that edge will be the shortest next path
+         *      to traverse along the graph.  For this graph we started
+         *      node "1" and the visible nodes (aka frontier nodes)
+         *      are "2=1, 3=7 & 4=5"; the next shortest path to traverse
+         *      will be node "2" at a distance of "1"
+         */
+        Enumeration el = hash.keys();
+        String key = null;
+        int val = 0;
+        int min = 999;
+        int max = 0;
+        while (el.hasMoreElements()) {
+            key = el.nextElement().toString();
+            val = hash.get(key);
+            System.out.println(key + " " + val);
+            max = Integer.parseInt(key);
+            if(min > max)
+            {
+                min = max;
+            }
+        }
+        return min;
     }
     /**
      * Print hashtable {String, Integer[]}
